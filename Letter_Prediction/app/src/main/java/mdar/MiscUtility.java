57640e61f0338d3173a950bbf791526213b8e863
Mason D'Arcy <masondarcy@gmail.com>
2021-03-30 12:48:37 -0400

    Update MiscUtility.java

--- Letter_Prediction/app/src/main/java/mdar/MiscUtility.java
+++ Letter_Prediction/app/src/main/java/mdar/MiscUtility.java
@@ -26,48 +26,6 @@ public final class MiscUtility {
         return "#FF000000";
     }
 
-
-    //This can be improved or simplified
-    public static String probabilityToColor(float prob) {
-        String output = "#FF";
-        Log.i("DEBUG", prob + " is the probability");
-        float whiteLine = 0.03846F;
-        int red = prob <= whiteLine ? 100 : (int) (255 * (1  / (1.5 + prob)));
-        int green = prob >= whiteLine ? 255 : (int) (255 * prob);
-        int blue = (int) (prob <= whiteLine ? 255 * (prob / whiteLine) : 255 * (whiteLine / 1 + prob));
-        if(blue > 0) {
-            blue *= 0.35;
-        }
-        Log.i("DEBUG", "blue: " + blue);
-        if(red < 16) {
-            output += "0" + Integer.toHexString(red);
-        } else {
-            output += Integer.toHexString(red);
-        }
-
-        if(green < 16) {
-            output += "0" + Integer.toHexString(green);
-        } else {
-            output += Integer.toHexString(green);
-        }
-        if(blue < 16) {
-            output += "0" + Integer.toHexString(blue);
-        } else {
-            output += Integer.toHexString(blue);
-        }
-
-        if(!Float.isNaN(prob)) {
-            Log.i("DEBUG", "Color: " + output);
-
-            return output;
-        } else{
-            return  "#FFFF0000";
-        }
-    }
-
-
-
-
     public static char mapNumberToLetter(int i) {
         return (char) (65 + i);
     }
